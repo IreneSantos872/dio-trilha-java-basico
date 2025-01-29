8fa469890f6161c47ecb27e810e904ccca30813b
IreneSantos872 <ireneptc@hotmail.com>
2025-01-28 22:39:07 -0300

    Segundo Commit

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+       //Exibir as mensagens para o nosso usuário
+        System.out.print("Por favor informe o numero da conta: ");
+        //Obter pela scanner os valores digitados no terminal
+        Integer numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero da agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo ");
+        Double saldo = scanner.nextDouble();
+
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco," +
+        "sua agência é " + agencia + " conta "+ numero + " e seu saldo " + saldo + " já está disponivel para saque.");
+        scanner.close();
     }
 }
